334890b3642eb0c307e20f10464ab27ace729cc7
CriBlight <47996373+CriBlight@users.noreply.github.com>
2019-04-04 18:28:47 +1100

    Started on TIO menu
    Started on a menu
    gitignore just contains config and preference files.

--- main.c
+++ main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+//Current Build = Tio
+//Auther: Christopher Baita C33289270
 
-main()
+int main()
 {
-    printf("First Lab Commit");
+    int opt = 0;
+    do 
+    {
+
+        printf("Please Select an option from below: \n 1: Encryption using roation cipher\n 2: Decryption using roation cipher\n 0: Exit \nInput: ");
+        scanf("%d", &opt);
+        if(opt > 0 && opt < 9) //9 is currently a placeholder until amount of options is determined.
+        {
+            switch(opt)
+            {
+                case 1: printf("case 1\n"); //funtion for 1 here
+                break;
+                case 2: printf("case 2\n"); //funtion 2 here
+                break;
+                //default: printf("Invaild Option\n");
+            }
+        }
+        else if(opt == 0)
+        {
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            printf("Invalid Input please select an option from the options provided\n");
+        }
+        
+    } while(1);
     return 0;
 }
