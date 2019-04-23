e9fe87f32d5b58e06e7a49bb508921f1c4295006
CriBlight <47996373+CriBlight@users.noreply.github.com>
2019-04-23 16:58:35 +1000

    subsituation E&D done
    (without fancy extra mark things)

--- main.c
+++ main.c
@@ -152,17 +152,259 @@ void rDecrypt() //new funtion, new troubles.
 
 void sEncrypt()
 {
-    char eMsg[1024], fMsg[1024], dMsg[1024], spce[] = {" "};
-    int key, i = 0;
+    FILE *in, *out; //add a file io for key
+    char eMsg[1024], fMsg[1024], dMsg[1024], spce[] = {" "}, key[] = {'U','W','G','T','L','Z','Y','X','J','M','A','K','S','B','O','C','I','N','Q','P','E','V','F','H','D','R','\0'}; //KEY = uwgtlzyxjmaksbocinqpevfhdr
+    int i = 0;
     
     memset(eMsg,0,strlen(eMsg));
     memset(dMsg,0,strlen(dMsg));
     memset(fMsg,0,strlen(fMsg));
 
-    printf("Please enter key\n");
-    scanf("%d", &key);
+    in = fopen("sIn.txt", "r");
 
-    key = 26 % key;
+    if(in == NULL)
+    {
+        perror("fopen()");
+        return;
+    }
+
+    strcat(fMsg, eMsg);
+
+    while(!feof(in))
+    {
+        fscanf(in, "%s", eMsg);
+        strcat(fMsg, eMsg);
+        strcat(fMsg, spce);
+    }
+
+    printf("Input is : %s", fMsg);
+
+    while(fMsg[i] != '\0')
+    {
+        if(fMsg[i] >= 97 && fMsg[i] <= 122)
+        {
+            fMsg[i] -= 32;
+        }
+        
+        switch(fMsg[i])
+        {
+            case 'A' : dMsg[i] = key[0];
+            break;
+            case 'B' : dMsg[i] = key[1];
+            break;
+            case 'C' : dMsg[i] = key[2];
+            break;
+            case 'D' : dMsg[i] = key[3];
+            break;
+            case 'E' : dMsg[i] = key[4];
+            break;
+            case 'F' : dMsg[i] = key[5];
+            break;
+            case 'G' : dMsg[i] = key[6];
+            break;
+            case 'H' : dMsg[i] = key[7];
+            break;
+            case 'I' : dMsg[i] = key[8];
+            break;
+            case 'J' : dMsg[i] = key[9];
+            break;
+            case 'K' : dMsg[i] = key[10];
+            break;
+            case 'L' : dMsg[i] = key[11];
+            break;
+            case 'M' : dMsg[i] = key[12];
+            break;
+            case 'N' : dMsg[i] = key[13];
+            break;
+            case 'O' : dMsg[i] = key[14];
+            break;
+            case 'P' : dMsg[i] = key[15];
+            break;
+            case 'Q' : dMsg[i] = key[16];
+            break;
+            case 'R' : dMsg[i] = key[17];
+            break;
+            case 'S' : dMsg[i] = key[18];
+            break;
+            case 'T' : dMsg[i] = key[19];
+            break;
+            case 'U' : dMsg[i] = key[20];
+            break;
+            case 'V' : dMsg[i] = key[21];
+            break;
+            case 'W' : dMsg[i] = key[22];
+            break;
+            case 'X' : dMsg[i] = key[23];
+            break;
+            case 'Y' : dMsg[i] = key[24];
+            break;
+            case 'Z' : dMsg[i] = key[25];
+            break;
+            default : dMsg[i] = fMsg[i];
+            break;
+        }
+        i++;
+    }
+    out = fopen("sOut.txt", "w");
+    fseek(out, 0, SEEK_SET);
+    fprintf(out, "%s\n", dMsg);
+    printf("\nYour encrypted message is %s\n", dMsg);
+    fclose(in);
+    fclose(out);
+}
+
+void sDecrypt()
+{
+    FILE *in, *out; //add a file io for key
+    char eMsg[1024], fMsg[1024], dMsg[1024], spce[] = {" "}, key[] = {'U','W','G','T','L','Z','Y','X','J','M','A','K','S','B','O','C','I','N','Q','P','E','V','F','H','D','R','\0'}; //KEY = uwgtlzyxjmaksbocinqpevfhdr
+    int i = 0;
+    
+    memset(eMsg,0,strlen(eMsg));
+    memset(dMsg,0,strlen(dMsg));
+    memset(fMsg,0,strlen(fMsg));
+
+    in = fopen("sdIn.txt", "r");
+
+    if(in == NULL)
+    {
+        perror("fopen()");
+        return;
+    }
+
+    strcat(fMsg, eMsg);
+
+    while(!feof(in))
+    {
+        fscanf(in, "%s", eMsg);
+        strcat(fMsg, eMsg);
+        strcat(fMsg, spce);
+    }
+
+    printf("Input is : %s", fMsg);
+
+    while(fMsg[i] != '\0')
+    {
+        if(fMsg[i] >= 97 && fMsg[i] <= 122)
+        {
+            fMsg[i] -= 32;
+        }
+        
+        if(fMsg[i] == key[0])
+        {
+            dMsg[i] = 'A';
+        }
+        else if (fMsg[i] == key[1])
+        {
+            dMsg[i] = 'B';
+        }
+        else if (fMsg[i] == key[2])
+        {
+            dMsg[i] = 'C';
+        }
+        else if (fMsg[i] == key[3])
+        {
+            dMsg[i] = 'D';
+        }
+        else if (fMsg[i] == key[4])
+        {
+            dMsg[i] = 'E';
+        }
+        else if (fMsg[i] == key[5])
+        {
+            dMsg[i] = 'F';
+        }
+        else if (fMsg[i] == key[6])
+        {
+            dMsg[i] = 'G';
+        }
+        else if (fMsg[i] == key[7])
+        {
+            dMsg[i] = 'H';
+        }
+        else if (fMsg[i] == key[8])
+        {
+            dMsg[i] = 'I';
+        }
+        else if (fMsg[i] == key[9])
+        {
+            dMsg[i] = 'J';
+        }
+        else if (fMsg[i] == key[10])
+        {
+            dMsg[i] = 'K';
+        }
+        else if (fMsg[i] == key[11])
+        {
+            dMsg[i] = 'L';
+        }
+        else if (fMsg[i] == key[12])
+        {
+            dMsg[i] = 'M';
+        }
+        else if (fMsg[i] == key[13])
+        {
+            dMsg[i] = 'N';
+        }
+        else if (fMsg[i] == key[14])
+        {
+            dMsg[i] = 'O';
+        }
+        else if (fMsg[i] == key[15])
+        {
+            dMsg[i] = 'P';
+        }
+        else if (fMsg[i] == key[16])
+        {
+            dMsg[i] = 'Q';
+        }
+        else if (fMsg[i] == key[17])
+        {
+            dMsg[i] = 'R';
+        }
+        else if (fMsg[i] == key[18])
+        {
+            dMsg[i] = 'S';
+        }
+        else if (fMsg[i] == key[19])
+        {
+            dMsg[i] = 'T';
+        }
+        else if (fMsg[i] == key[20])
+        {
+            dMsg[i] = 'U';
+        }
+        else if (fMsg[i] == key[21])
+        {
+            dMsg[i] = 'V';
+        }
+        else if (fMsg[i] == key[22])
+        {
+            dMsg[i] = 'W';
+        }
+        else if (fMsg[i] == key[23])
+        {
+            dMsg[i] = 'X';
+        }
+        else if (fMsg[i] == key[24])
+        {
+            dMsg[i] = 'Y';
+        }
+        else if (fMsg[i] == key[25])
+        {
+            dMsg[i] = 'Z';
+        }
+        else
+        {
+            dMsg[i] = fMsg[i];
+        }
+        i++;
+    }
+    out = fopen("sdOut.txt", "w");
+    fseek(out, 0, SEEK_SET);
+    fprintf(out, "%s\n", dMsg);
+    printf("\nYour decrypted message is %s\n", dMsg);
+    fclose(in);
+    fclose(out);
 }
 
 int main()
@@ -181,6 +423,10 @@ int main()
                 case 1: rEncrypt(); //funtion for 1 here
                 break;
                 case 2: rDecrypt(); //funtion 2 here
+                break;
+                case 3: sEncrypt();
+                break;
+                case 4: sDecrypt();
                 break;
                 //default: printf("Invaild Option\n");
             }
